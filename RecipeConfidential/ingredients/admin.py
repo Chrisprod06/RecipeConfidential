1d44656695a0eddba73cbda891b5f2ba996a58f9
chris <chrisprodromou06@gmail.com>
2022-09-05 22:58:15 +0300

    progress to ingredients

--- RecipeConfidential/ingredients/admin.py
+++ RecipeConfidential/ingredients/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from ingredients.models import *
 
-# Register your models here.
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "measurement", "quantity", "extra_notes")
+    list_filter = ("category",)
+    search_fields = ("name",)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("key", "description")
+
+
+@admin.register(Measurement)
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = ("key", "name")
--- RecipeConfidential/ingredients/models.py
+++ RecipeConfidential/ingredients/models.py
@@ -1,4 +1,53 @@
 from django.db import models
 
-# Create your models here.
 
+class Category(models.Model):
+    """
+    Model to describe categories of ingredients
+    """
+    # Key
+    key = models.CharField(max_length=30, blank=False, unique=True)
+    # Description
+    description = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+    def __str__(self):
+        return self.key
+
+
+class Measurement(models.Model):
+    """
+    Model to describe type of measurements of ingredients
+    """
+    # key
+    key = models.CharField(max_length=10)
+    # Name
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.key
+
+
+class Ingredient(models.Model):
+    """
+    Model to describe ingredients that will be used in the recipes
+    """
+    # Name
+    name = models.CharField(max_length=255)
+
+    # Category
+    category = models.ForeignKey(Category, on_delete=models.RESTRICT)
+
+    # Measurement
+    measurement = models.ForeignKey(Measurement, on_delete=models.RESTRICT)
+
+    # Quantity
+    quantity = models.FloatField(blank=False)
+
+    # Extra notes
+    extra_notes = models.CharField(max_length=500, blank=True)
+
+    def __str__(self):
+        return self.name
